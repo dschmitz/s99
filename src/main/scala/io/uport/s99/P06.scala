36717847b89fec87f658192f2ea38e8feb7ea11f
David Schmitz <David-Schmitz@gmx.net>
2017-12-25 21:55:23 +0100

    added P06

---
+++ src/main/scala/io/uport/s99/P06.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 David Schmitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.uport.s99
+
+/*
+P06 (*) Find out whether a list is a palindrome.
+Example:
+scala> isPalindrome(List(1, 2, 3, 2, 1))
+res0: Boolean = true
+ */
+object P06 {
+
+  def isPalindromeBuiltin(list: List[Int]): Boolean = list == list.reverse
+
+  def isPalindromeTailRecursive(list: List[Int]): Boolean = list == P05.reverseBuiltin(list)
+
+  def isPalindromeTailFunctional(list: List[Int]): Boolean = list == P05.reverseFunctional(list)
+
+}
---
+++ src/test/scala/io/uport/s99/P06Spec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 David Schmitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.uport.s99
+
+class P06Spec extends ProblemSpec {
+
+  def tests(testName: String, ls: List[Int] => Boolean): Unit = {
+    testName should "return true single element's list" in {
+      ls(List(3)) should equal(true)
+    }
+
+    it should "return false for empty list" in {
+      ls(List()) should be(true)
+    }
+
+    it should "return true for palindrome list" in {
+      ls(List(1, 2, 3, 2, 1)) should be(true)
+    }
+  }
+
+  tests("IsPalindromeBuiltin", P06.isPalindromeBuiltin)
+}
