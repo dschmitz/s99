fdda7991d8a0e9fd38f2832bbb22e4b24766e954
David Schmitz <David-Schmitz@gmx.net>
2018-01-02 19:49:57 +0100

    formatted source codformatted source code

--- src/main/scala/io/uport/s99/P49.scala
+++ src/main/scala/io/uport/s99/P49.scala
@@ -1,3 +1,30 @@
+/*
+ * Copyright 2017 David Schmitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.uport.s99
 
+/*
+P49 (**) Gray code.
+An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
+n = 1: C(1) = ("0", "1").
+n = 2: C(2) = ("00", "01", "11", "10").
+n = 3: C(3) = ("000", "001", "011", "010", "110", "111", "101", "100").
+Find out the construction rules and write a function to generate Gray codes.
+scala> gray(3)
+res0 List[String] = List(000, 001, 011, 010, 110, 111, 101, 100)
+See if you can use memoization to make the function more efficient.
+ */
 object P49 {}
--- src/main/scala/io/uport/s99/P50.scala
+++ src/main/scala/io/uport/s99/P50.scala
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2017 David Schmitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.uport.s99
 
+/*
+P50 (***) Huffman code.
+First of all, consult a good book on discrete mathematics or algorithms for a detailed description of Huffman codes!
+We suppose a set of symbols with their frequencies, given as a list of (S, F) Tuples. E.g. (("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9),
+("f", 5)). Our objective is to construct a list of (S, C) Tuples, where C is the Huffman code word for the symbol S.
+scala> huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)))
+res0: List[String, String] = List((a,0), (b,101), (c,100), (d,111), (e,1101), (f,1100))
+ */
 object P50 {}
